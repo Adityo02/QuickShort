441cea69444677b8a78b439acec00b63b05ac5ff
Adityo Arya Putra <adityo.arya.ft21@mail.umy.ac.id>
2022-10-17 17:12:29 +0700

    create void swap

--- QuickShort/Program.cs
+++ QuickShort/Program.cs
@@ -41,7 +41,15 @@ namespace QuickShort
                 arr[i] = Int32.Parse(s1);
             }
         }
-        
+        //swaps the element at index x with the element at index y
+        void swap(int x, int y)
+        {
+            int temp;
+
+            temp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = temp;
+        }
         static void Main(string[] args)
         {
         }
