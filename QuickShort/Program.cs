d96212fb98d70e7863797422e8b28bacf317f360
Adityo Arya Putra <adityo.arya.ft21@mail.umy.ac.id>
2022-10-17 17:55:16 +0700

    Accept array elemets

--- QuickShort/Program.cs
+++ QuickShort/Program.cs
@@ -125,6 +125,8 @@ namespace QuickShort
         {
             //Declaring the object of the class
             Program myList = new Program();
+            //Accept array elemets
+            myList.read();
 
         }
     }
