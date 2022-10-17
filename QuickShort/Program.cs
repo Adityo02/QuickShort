c8971d686dc87042e69ec7df88535a9a06ce0814
Adityo Arya Putra <adityo.arya.ft21@mail.umy.ac.id>
2022-10-17 17:25:04 +0700

    Search for an element less than or equel to pivot

--- QuickShort/Program.cs
+++ QuickShort/Program.cs
@@ -74,6 +74,14 @@ namespace QuickShort
                     cmp_count++;
                 }
                 cmp_count++;
+
+                //Search for an element less than or equel to pivot
+                while ((arr[j] > pivot) && (j >= low))
+                {
+                    j--;
+                    cmp_count++;
+                }
+                cmp_count++;
             }
         }
         static void Main(string[] args)
