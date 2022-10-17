e3079ede75e1ddfd931edb2ee8e09e787e369b71
Adityo Arya Putra <adityo.arya.ft21@mail.umy.ac.id>
2022-10-17 16:55:15 +0700

    start

--- QuickShort/Program.cs
+++ QuickShort/Program.cs
@@ -10,6 +10,9 @@ namespace QuickShort
     {
         //array of integers to hold values
         private int[] arr = new int[20];
+        private int cmp_count = 0; // number of comparasion
+        private int mov_count = 0; // number of data movements
+
         static void Main(string[] args)
         {
         }
