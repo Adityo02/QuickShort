6889d1bb083bff896e1fc06ab6d1a44006153260
Adityo Arya Putra <adityo.arya.ft21@mail.umy.ac.id>
2022-10-17 17:57:29 +0700

    Display sorted array

--- QuickShort/Program.cs
+++ QuickShort/Program.cs
@@ -130,6 +130,8 @@ namespace QuickShort
             //Calling the sorting function
             //Frist call to quicksort algorithm
             myList.q_sort(0, myList.getSize() - 1);
+            //Display sorted array
+            myList.display();
         }
     }
 }
